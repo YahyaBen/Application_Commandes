bae57ca5c8d32261562917e55cdb7df3813485ff
BENABDALLAH Yahya <yahya.ben.abdallah@gmail.com>
2020-01-07 12:30:38 +0100

    Clients Form

---
+++ Application Commandes/Clients.Designer.cs
@@ -0,0 +1,296 @@
+﻿namespace Application_Commandes
+{
+    partial class Clients
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Btn_CliAjouter = new System.Windows.Forms.Button();
+            this.Btn_CliModifier = new System.Windows.Forms.Button();
+            this.Btn_CliSupprimer = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.DGV_Cli_1 = new System.Windows.Forms.DataGridView();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.DGV_Cli_2 = new System.Windows.Forms.DataGridView();
+            this.label7 = new System.Windows.Forms.Label();
+            this.CRUD_Client = new System.Windows.Forms.GroupBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.Btn_CliFermer = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.DGV_Cli_1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DGV_Cli_2)).BeginInit();
+            this.CRUD_Client.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Btn_CliAjouter
+            // 
+            this.Btn_CliAjouter.Location = new System.Drawing.Point(289, 28);
+            this.Btn_CliAjouter.Name = "Btn_CliAjouter";
+            this.Btn_CliAjouter.Size = new System.Drawing.Size(75, 23);
+            this.Btn_CliAjouter.TabIndex = 0;
+            this.Btn_CliAjouter.Text = "Ajouter";
+            this.Btn_CliAjouter.UseVisualStyleBackColor = true;
+            // 
+            // Btn_CliModifier
+            // 
+            this.Btn_CliModifier.Location = new System.Drawing.Point(289, 57);
+            this.Btn_CliModifier.Name = "Btn_CliModifier";
+            this.Btn_CliModifier.Size = new System.Drawing.Size(75, 23);
+            this.Btn_CliModifier.TabIndex = 1;
+            this.Btn_CliModifier.Text = "Modifier";
+            this.Btn_CliModifier.UseVisualStyleBackColor = true;
+            // 
+            // Btn_CliSupprimer
+            // 
+            this.Btn_CliSupprimer.Location = new System.Drawing.Point(289, 86);
+            this.Btn_CliSupprimer.Name = "Btn_CliSupprimer";
+            this.Btn_CliSupprimer.Size = new System.Drawing.Size(75, 23);
+            this.Btn_CliSupprimer.TabIndex = 2;
+            this.Btn_CliSupprimer.Text = "Supprimer";
+            this.Btn_CliSupprimer.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(24, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "ID :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Nom :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 89);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Prenom :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(22, 17);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(66, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Recherche :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(22, 43);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(76, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Chiffre Affaire :";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(22, 69);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(92, 13);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Nbr Totale Client :";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(69, 34);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(168, 20);
+            this.textBox1.TabIndex = 9;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(69, 60);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(168, 20);
+            this.textBox2.TabIndex = 10;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(69, 86);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(168, 20);
+            this.textBox3.TabIndex = 11;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(116, 10);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(146, 20);
+            this.textBox4.TabIndex = 12;
+            // 
+            // DGV_Cli_1
+            // 
+            this.DGV_Cli_1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DGV_Cli_1.Location = new System.Drawing.Point(30, 259);
+            this.DGV_Cli_1.Name = "DGV_Cli_1";
+            this.DGV_Cli_1.Size = new System.Drawing.Size(379, 146);
+            this.DGV_Cli_1.TabIndex = 13;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(116, 36);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(146, 20);
+            this.textBox5.TabIndex = 14;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(116, 62);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(146, 20);
+            this.textBox6.TabIndex = 15;
+            // 
+            // DGV_Cli_2
+            // 
+            this.DGV_Cli_2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DGV_Cli_2.Location = new System.Drawing.Point(30, 427);
+            this.DGV_Cli_2.Name = "DGV_Cli_2";
+            this.DGV_Cli_2.Size = new System.Drawing.Size(379, 94);
+            this.DGV_Cli_2.TabIndex = 16;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(27, 408);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(79, 13);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "Commentaires :";
+            // 
+            // CRUD_Client
+            // 
+            this.CRUD_Client.Controls.Add(this.textBox3);
+            this.CRUD_Client.Controls.Add(this.Btn_CliAjouter);
+            this.CRUD_Client.Controls.Add(this.Btn_CliModifier);
+            this.CRUD_Client.Controls.Add(this.Btn_CliSupprimer);
+            this.CRUD_Client.Controls.Add(this.label1);
+            this.CRUD_Client.Controls.Add(this.label2);
+            this.CRUD_Client.Controls.Add(this.label3);
+            this.CRUD_Client.Controls.Add(this.textBox1);
+            this.CRUD_Client.Controls.Add(this.textBox2);
+            this.CRUD_Client.Location = new System.Drawing.Point(30, 12);
+            this.CRUD_Client.Name = "CRUD_Client";
+            this.CRUD_Client.Size = new System.Drawing.Size(379, 135);
+            this.CRUD_Client.TabIndex = 0;
+            this.CRUD_Client.TabStop = false;
+            this.CRUD_Client.Text = "Client";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.textBox4);
+            this.groupBox1.Controls.Add(this.textBox6);
+            this.groupBox1.Controls.Add(this.textBox5);
+            this.groupBox1.Location = new System.Drawing.Point(30, 153);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(300, 100);
+            this.groupBox1.TabIndex = 18;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Information";
+            // 
+            // Btn_CliFermer
+            // 
+            this.Btn_CliFermer.Location = new System.Drawing.Point(336, 178);
+            this.Btn_CliFermer.Name = "Btn_CliFermer";
+            this.Btn_CliFermer.Size = new System.Drawing.Size(75, 49);
+            this.Btn_CliFermer.TabIndex = 12;
+            this.Btn_CliFermer.Text = "Fermer";
+            this.Btn_CliFermer.UseVisualStyleBackColor = true;
+            // 
+            // Clients
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(441, 533);
+            this.Controls.Add(this.Btn_CliFermer);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.CRUD_Client);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.DGV_Cli_2);
+            this.Controls.Add(this.DGV_Cli_1);
+            this.Name = "Clients";
+            this.Text = "Clients";
+            ((System.ComponentModel.ISupportInitialize)(this.DGV_Cli_1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DGV_Cli_2)).EndInit();
+            this.CRUD_Client.ResumeLayout(false);
+            this.CRUD_Client.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Btn_CliAjouter;
+        private System.Windows.Forms.Button Btn_CliModifier;
+        private System.Windows.Forms.Button Btn_CliSupprimer;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.DataGridView DGV_Cli_1;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.DataGridView DGV_Cli_2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.GroupBox CRUD_Client;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button Btn_CliFermer;
+    }
+}
--- Application Commandes/Form1.Designer.cs
+++ Application Commandes/Form1.Designer.cs
@@ -28,20 +28,162 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.Btn_CatAjouter = new System.Windows.Forms.Button();
+            this.Btn_CatModifier = new System.Windows.Forms.Button();
+            this.Btn_CatSupprimer = new System.Windows.Forms.Button();
+            this.Btn_CatRechercher = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Txt_CatID = new System.Windows.Forms.TextBox();
+            this.Txt_CatLibelle = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Btn_CatFermer = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Txt_CatNbrProuit = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // Btn_CatAjouter
+            // 
+            this.Btn_CatAjouter.Location = new System.Drawing.Point(390, 27);
+            this.Btn_CatAjouter.Name = "Btn_CatAjouter";
+            this.Btn_CatAjouter.Size = new System.Drawing.Size(75, 23);
+            this.Btn_CatAjouter.TabIndex = 0;
+            this.Btn_CatAjouter.Text = "Ajouter";
+            this.Btn_CatAjouter.UseVisualStyleBackColor = true;
+            // 
+            // Btn_CatModifier
+            // 
+            this.Btn_CatModifier.Location = new System.Drawing.Point(390, 56);
+            this.Btn_CatModifier.Name = "Btn_CatModifier";
+            this.Btn_CatModifier.Size = new System.Drawing.Size(75, 23);
+            this.Btn_CatModifier.TabIndex = 1;
+            this.Btn_CatModifier.Text = "Modifier";
+            this.Btn_CatModifier.UseVisualStyleBackColor = true;
+            // 
+            // Btn_CatSupprimer
+            // 
+            this.Btn_CatSupprimer.Location = new System.Drawing.Point(390, 85);
+            this.Btn_CatSupprimer.Name = "Btn_CatSupprimer";
+            this.Btn_CatSupprimer.Size = new System.Drawing.Size(75, 23);
+            this.Btn_CatSupprimer.TabIndex = 2;
+            this.Btn_CatSupprimer.Text = "Supprimer";
+            this.Btn_CatSupprimer.UseVisualStyleBackColor = true;
+            // 
+            // Btn_CatRechercher
+            // 
+            this.Btn_CatRechercher.Location = new System.Drawing.Point(390, 114);
+            this.Btn_CatRechercher.Name = "Btn_CatRechercher";
+            this.Btn_CatRechercher.Size = new System.Drawing.Size(75, 23);
+            this.Btn_CatRechercher.TabIndex = 3;
+            this.Btn_CatRechercher.Text = "Rechercher";
+            this.Btn_CatRechercher.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(24, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "ID :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(37, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Libelle :";
+            // 
+            // Txt_CatID
+            // 
+            this.Txt_CatID.Location = new System.Drawing.Point(105, 30);
+            this.Txt_CatID.Name = "Txt_CatID";
+            this.Txt_CatID.Size = new System.Drawing.Size(202, 20);
+            this.Txt_CatID.TabIndex = 8;
+            // 
+            // Txt_CatLibelle
+            // 
+            this.Txt_CatLibelle.Location = new System.Drawing.Point(105, 73);
+            this.Txt_CatLibelle.Name = "Txt_CatLibelle";
+            this.Txt_CatLibelle.Size = new System.Drawing.Size(202, 20);
+            this.Txt_CatLibelle.TabIndex = 9;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(40, 114);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(313, 209);
+            this.dataGridView1.TabIndex = 12;
+            // 
+            // Btn_CatFermer
+            // 
+            this.Btn_CatFermer.Location = new System.Drawing.Point(390, 270);
+            this.Btn_CatFermer.Name = "Btn_CatFermer";
+            this.Btn_CatFermer.Size = new System.Drawing.Size(75, 53);
+            this.Btn_CatFermer.TabIndex = 13;
+            this.Btn_CatFermer.Text = "Fermer";
+            this.Btn_CatFermer.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(359, 187);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 13);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Nbr Produits :";
+            // 
+            // Txt_CatNbrProuit
+            // 
+            this.Txt_CatNbrProuit.Enabled = false;
+            this.Txt_CatNbrProuit.Location = new System.Drawing.Point(436, 180);
+            this.Txt_CatNbrProuit.Name = "Txt_CatNbrProuit";
+            this.Txt_CatNbrProuit.Size = new System.Drawing.Size(29, 20);
+            this.Txt_CatNbrProuit.TabIndex = 15;
             // 
             // Categories
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(523, 450);
+            this.ClientSize = new System.Drawing.Size(508, 341);
+            this.Controls.Add(this.Txt_CatNbrProuit);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Btn_CatFermer);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.Txt_CatLibelle);
+            this.Controls.Add(this.Txt_CatID);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Btn_CatRechercher);
+            this.Controls.Add(this.Btn_CatSupprimer);
+            this.Controls.Add(this.Btn_CatModifier);
+            this.Controls.Add(this.Btn_CatAjouter);
             this.Name = "Categories";
             this.Text = "Categories";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button Btn_CatAjouter;
+        private System.Windows.Forms.Button Btn_CatModifier;
+        private System.Windows.Forms.Button Btn_CatSupprimer;
+        private System.Windows.Forms.Button Btn_CatRechercher;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox Txt_CatID;
+        private System.Windows.Forms.TextBox Txt_CatLibelle;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button Btn_CatFermer;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox Txt_CatNbrProuit;
     }
 }
 
